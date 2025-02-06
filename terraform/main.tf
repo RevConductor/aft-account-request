6c4cf6cdf00406aa70399ca7dfb0bb31b8625c5f
thebuck25 <chris@revconductor.com>
2025-02-06 10:15:06 -0700

    ci: deploy new accounts

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,23 @@
 module "sandbox" {
-  source = "./modules/aft-account-request"
+  # Single Environment - create new accounts in variables.tf
+  # AND BE CAREFUL
+  for_each = var.accounts
+  source   = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    AccountName               = each.key
+    AccountEmail              = each.value.email
+    ManagedOrganizationalUnit = each.value.managed_ou
+    SSOUserEmail              = each.value.sso_email
+    SSOUserFirstName          = each.value.first_name
+    SSOUserLastName           = each.value.last_name
   }
 
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
+  account_customizations_name = each.key
+  account_tags                = each.value.tags
+  custom_fields               = each.value.custom_fields
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+    change_requested_by = var.requested_by
+    change_reason       = each.value.change_reason
   }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sandbox"
 }
